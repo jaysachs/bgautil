ffd3b327dc149e8b81acc2b971561e419064a1d2
Jay Sachs <jaysachs@users.noreply.github.com>
2025-09-18 08:38:00 -0400

    Modernize test

--- genstats/StatsTest.php
+++ genstats/StatsTest.php
@@ -18,11 +18,21 @@
 
 declare(strict_types=1);
 
+namespace Bga\GameFramework {
+
+class Table {}
+
+}
+
+
+namespace {
+
 use PHPUnit\Framework\TestCase;
 
 use Bga\Games\testgame\Stats;
 
-class FakeImpl {
+
+class FakeImpl extends Bga\GameFramework\Table {
     private $vals = [];
 
     public function initStat($cat, $name, $value, $player_id = null) {
@@ -72,105 +82,108 @@ final class StatsTest extends TestCase
 {
     const array PLAYER_IDS = [ 5, 7 ];
     private ?FakeImpl $impl;
+    private Stats $stats;
 
     protected function setUp(): void {
         $this->impl = new FakeImpl();
-        Stats::init($this->impl);
+        $this->stats = Stats::createForGame($this->impl);
     }
 
     public function testInitAll(): void {
-        Stats::initAll(self::PLAYER_IDS);
+        $this->stats->initAll(self::PLAYER_IDS);
 
-        $this->assertEquals(0, Stats::PLAYER_NUMBER_TURNS->get(5));
-        $this->assertEquals(0, Stats::PLAYER_NUMBER_TURNS->get(7));
-        $this->assertEquals(false, Stats::PLAYER_MET_OBJECTIVE->get(5));
-        $this->assertEquals(false, Stats::PLAYER_MET_OBJECTIVE->get(7));
-        $this->assertEquals(0.0, Stats::PLAYER_AVERAGE_DIE_ROLL->get(5));
-        $this->assertEquals(0.0, Stats::PLAYER_AVERAGE_DIE_ROLL->get(7));
+        $this->assertEquals(0, $this->stats->PLAYER_NUMBER_TURNS->get(5));
+        $this->assertEquals(0, $this->stats->PLAYER_NUMBER_TURNS->get(7));
+        $this->assertEquals(false, $this->stats->PLAYER_MET_OBJECTIVE->get(5));
+        $this->assertEquals(false, $this->stats->PLAYER_MET_OBJECTIVE->get(7));
+        $this->assertEquals(0.0, $this->stats->PLAYER_AVERAGE_DIE_ROLL->get(5));
+        $this->assertEquals(0.0, $this->stats->PLAYER_AVERAGE_DIE_ROLL->get(7));
 
-        $this->assertEquals(0, Stats::TABLE_CITIES_CAPTURED->get());
-        $this->assertEquals(false, Stats::TABLE_SUDDEN_DEATH->get());
-        $this->assertEquals(0.0, Stats::TABLE_AVERAGE_PIECES_PER_TURN->get());
+        $this->assertEquals(0, $this->stats->TABLE_CITIES_CAPTURED->get());
+        $this->assertEquals(false, $this->stats->TABLE_SUDDEN_DEATH->get());
+        $this->assertEquals(0.0, $this->stats->TABLE_AVERAGE_PIECES_PER_TURN->get());
     }
 
     public function testInitMap(): void {
-        Stats::PLAYER_NUMBER_TURNS->initMap(self::PLAYER_IDS,
+        $this->stats->PLAYER_NUMBER_TURNS->initMap(self::PLAYER_IDS,
                                             function (int $p) { return $p*$p+1; });
-        $this->assertEquals(26, Stats::PLAYER_NUMBER_TURNS->get(5));
-        $this->assertEquals(50, Stats::PLAYER_NUMBER_TURNS->get(7));
+        $this->assertEquals(26, $this->stats->PLAYER_NUMBER_TURNS->get(5));
+        $this->assertEquals(50, $this->stats->PLAYER_NUMBER_TURNS->get(7));
     }
 
     public function testPlayerInt(): void {
-        Stats::initAll(self::PLAYER_IDS);
+        $this->stats->initAll(self::PLAYER_IDS);
 
-        Stats::PLAYER_NUMBER_TURNS->set(5, 4);
+        $this->stats->PLAYER_NUMBER_TURNS->set(5, 4);
 
-        $this->assertEquals(4, Stats::PLAYER_NUMBER_TURNS->get(5));
-        $this->assertEquals(0, Stats::PLAYER_NUMBER_TURNS->get(7));
+        $this->assertEquals(4, $this->stats->PLAYER_NUMBER_TURNS->get(5));
+        $this->assertEquals(0, $this->stats->PLAYER_NUMBER_TURNS->get(7));
 
-        Stats::PLAYER_NUMBER_TURNS->inc(7);
-        Stats::PLAYER_NUMBER_TURNS->inc(7);
-        Stats::PLAYER_NUMBER_TURNS->inc(7, 3);
+        $this->stats->PLAYER_NUMBER_TURNS->inc(7);
+        $this->stats->PLAYER_NUMBER_TURNS->inc(7);
+        $this->stats->PLAYER_NUMBER_TURNS->inc(7, 3);
 
-        $this->assertEquals(4, Stats::PLAYER_NUMBER_TURNS->get(5));
-        $this->assertEquals(5, Stats::PLAYER_NUMBER_TURNS->get(7));
+        $this->assertEquals(4, $this->stats->PLAYER_NUMBER_TURNS->get(5));
+        $this->assertEquals(5, $this->stats->PLAYER_NUMBER_TURNS->get(7));
     }
 
     public function testPlayerBool(): void {
-        Stats::initAll(self::PLAYER_IDS);
+        $this->stats->initAll(self::PLAYER_IDS);
 
-        Stats::PLAYER_MET_OBJECTIVE->set(5, true);
+        $this->stats->PLAYER_MET_OBJECTIVE->set(5, true);
 
-        $this->assertEquals(true, Stats::PLAYER_MET_OBJECTIVE->get(5));
-        $this->assertEquals(false, Stats::PLAYER_MET_OBJECTIVE->get(7));
+        $this->assertEquals(true, $this->stats->PLAYER_MET_OBJECTIVE->get(5));
+        $this->assertEquals(false, $this->stats->PLAYER_MET_OBJECTIVE->get(7));
     }
 
     public function testPlayerFloat(): void {
-        Stats::initAll(self::PLAYER_IDS);
+        $this->stats->initAll(self::PLAYER_IDS);
 
-        Stats::PLAYER_AVERAGE_DIE_ROLL->set(5, 1.732);
+        $this->stats->PLAYER_AVERAGE_DIE_ROLL->set(5, 1.732);
 
-        $this->assertEquals(1.732, Stats::PLAYER_AVERAGE_DIE_ROLL->get(5));
-        $this->assertEquals(0.0, Stats::PLAYER_AVERAGE_DIE_ROLL->get(7));
+        $this->assertEquals(1.732, $this->stats->PLAYER_AVERAGE_DIE_ROLL->get(5));
+        $this->assertEquals(0.0, $this->stats->PLAYER_AVERAGE_DIE_ROLL->get(7));
 
-        Stats::PLAYER_AVERAGE_DIE_ROLL->add(7, 3.0);
-        Stats::PLAYER_AVERAGE_DIE_ROLL->add(7, 3.14159);
+        $this->stats->PLAYER_AVERAGE_DIE_ROLL->add(7, 3.0);
+        $this->stats->PLAYER_AVERAGE_DIE_ROLL->add(7, 3.14159);
 
-        $this->assertEquals(1.732, Stats::PLAYER_AVERAGE_DIE_ROLL->get(5));
-        $this->assertEquals(6.14159, Stats::PLAYER_AVERAGE_DIE_ROLL->get(7));
+        $this->assertEquals(1.732, $this->stats->PLAYER_AVERAGE_DIE_ROLL->get(5));
+        $this->assertEquals(6.14159, $this->stats->PLAYER_AVERAGE_DIE_ROLL->get(7));
     }
 
     public function testTableInt(): void {
-        Stats::initAll(self::PLAYER_IDS);
+        $this->stats->initAll(self::PLAYER_IDS);
 
-        Stats::TABLE_CITIES_CAPTURED->set(3);
+        $this->stats->TABLE_CITIES_CAPTURED->set(3);
 
-        $this->assertEquals(3, Stats::TABLE_CITIES_CAPTURED->get(5));
+        $this->assertEquals(3, $this->stats->TABLE_CITIES_CAPTURED->get(5));
 
-        Stats::TABLE_CITIES_CAPTURED->inc();
-        Stats::TABLE_CITIES_CAPTURED->inc();
-        Stats::TABLE_CITIES_CAPTURED->inc(3);
-        $this->assertEquals(8, Stats::TABLE_CITIES_CAPTURED->get(5));
+        $this->stats->TABLE_CITIES_CAPTURED->inc();
+        $this->stats->TABLE_CITIES_CAPTURED->inc();
+        $this->stats->TABLE_CITIES_CAPTURED->inc(3);
+        $this->assertEquals(8, $this->stats->TABLE_CITIES_CAPTURED->get(5));
     }
 
     public function testTableBool(): void {
-        Stats::initAll(self::PLAYER_IDS);
+        $this->stats->initAll(self::PLAYER_IDS);
 
-        Stats::TABLE_SUDDEN_DEATH->set(true);
+        $this->stats->TABLE_SUDDEN_DEATH->set(true);
 
-        $this->assertEquals(true, Stats::TABLE_SUDDEN_DEATH->get());
+        $this->assertEquals(true, $this->stats->TABLE_SUDDEN_DEATH->get());
     }
 
     public function testTableFloat(): void {
-        Stats::initAll(self::PLAYER_IDS);
+        $this->stats->initAll(self::PLAYER_IDS);
 
-        Stats::TABLE_AVERAGE_PIECES_PER_TURN->set(2.1);
+        $this->stats->TABLE_AVERAGE_PIECES_PER_TURN->set(2.1);
 
-        $this->assertEquals(2.1, Stats::TABLE_AVERAGE_PIECES_PER_TURN->get());
+        $this->assertEquals(2.1, $this->stats->TABLE_AVERAGE_PIECES_PER_TURN->get());
 
-        Stats::TABLE_AVERAGE_PIECES_PER_TURN->add(3.0);
-        Stats::TABLE_AVERAGE_PIECES_PER_TURN->add(5.7);
+        $this->stats->TABLE_AVERAGE_PIECES_PER_TURN->add(3.0);
+        $this->stats->TABLE_AVERAGE_PIECES_PER_TURN->add(5.7);
 
-        $this->assertEquals(10.8, Stats::TABLE_AVERAGE_PIECES_PER_TURN->get());
+        $this->assertEquals(10.8, $this->stats->TABLE_AVERAGE_PIECES_PER_TURN->get());
     }
+}
+
 }
